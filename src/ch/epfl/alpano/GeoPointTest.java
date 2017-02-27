16036ce2ed0e4bf7ed6dd1caaed76102a4b77048
Louis Amaudruz <Louis Amaudruz>
2017-02-27 13:53:06 +0200

    more geopoint tests

--- src/ch/epfl/alpano/GeoPointTest.java
+++ src/ch/epfl/alpano/GeoPointTest.java
@@ -3,7 +3,7 @@ package ch.epfl.alpano;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
+import ch.epfl.alpano.Distance.*;
 
 public class GeoPointTest {
 
@@ -21,16 +21,26 @@ public class GeoPointTest {
     public void distanceToOnTrivial(){
         GeoPoint p = new GeoPoint(0.11573,0.81194);
         GeoPoint p2 = new GeoPoint(0.65665, 0.97307);
+        GeoPoint p3 = new GeoPoint(0.11573, 1);
         assertEquals(2370000, p.distanceTo(p2), 100);
+        assertEquals(0, p.distanceTo(p), 0);
+        assertEquals((1-0.81194)*Distance.EARTH_RADIUS,p.distanceTo(p3), 100 )
+
     }
     
     @Test
     public void azimuthToOnTrivial(){
         GeoPoint p = new GeoPoint(0.11573,0.81194);
         GeoPoint p2 = new GeoPoint(0.65665, 0.97307);
-        System.out.println(p2);
-        assertEquals(-52.95, p.azimuthTo(p2)*180/Math.PI, 0.01);
+        Geopoint p3 = new GeoPoint(0,0.81194)
+        Geopoint p4 = new Geopoint(0.11573, 0)
+        assertEquals(52.95, p.azimuthTo(p2)*180/Math.PI, 0.01);
+        assertEquals(0, p.azimuthTo(p) 0.01);
+        assertEquals(Math.PI/2, p.azimuthTo(p3), 0.01)
+        assertEquals(0, p.azimuthTo(p4), 0.01)
     }
+    
+   
     
     @Test 
     public void ToStringTest() {
